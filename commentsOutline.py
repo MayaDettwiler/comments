d53be54c993bc0f157f49c2677b8fecfcbe62649
MayaDettwiler <73507307+MayaDettwiler@users.noreply.github.com>
2020-10-28 14:09:19 -0500

    Add files via upload

---
+++ commentsOutline.py
@@ -0,0 +1,24 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+
+#applejuice
+
+
+#2) Make a multi-line comment.
+
+'''
+yeehaw
+wow
+cool
+
+'''
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+
+var = 5 #idkwhattowritelol
+
