0c78c6e3c57f9ee5efd947caaa6ec2b417aef013
MayaDettwiler <73507307+MayaDettwiler@users.noreply.github.com>
2020-10-28 14:14:21 -0500

    Add files via upload

---
+++ commentDrills.py
@@ -0,0 +1,46 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+
+#helloeveryone
+
+#2) Make a multi-line comment.
+
+'''
+yeah whatever
+idk what to write
+hehe
+
+'''
+
+#3) Make a variable. Then put a comment after it, on the same line.
+
+var = 20 #thisisthevariable
+
+#4) Make a single line comment.
+
+#writingacommentagain
+
+#5) Make a multi-line comment.
+
+'''
+hopefully I did this right
+idk what I'm doing
+I'm trying my best
+
+'''
